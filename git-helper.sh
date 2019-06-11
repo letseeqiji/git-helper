9e917a9db60b4731b131ff421d89bbc0ce20e942
imanner <wowiwo@yeah.net>
2019-06-11 17:57:06 +0800

    升级菜单显示

--- git-helper.sh
+++ git-helper.sh
@@ -46,26 +46,26 @@ if [[ -z $(git config --global --list | grep user.name) ]] && [[ -z $(git config
 fi
 
 git status
-options="[Add|Commit|Diff|Fetch|Exit|Help|Log|Push|User|Reset|Status]"
-while echo -e -n "\n\n\033[01;36m选择操作，输入要执行操作首字母即可.\n$options: \033[0m "
-	read -n1 choose
+options="[ 0-查看状态 | 1-添加修改 | 2-提交修改 | 3-比较文件 | 4-拉取文件 | 5-查看日志 | 6-推送修改 ]\n[ 7-运行命令 | 8-回滚操作 | 9-删除文件 | A-文件改名 | E-退出命令 | H-帮助文档 ]"
+while echo -e -n "\n\n\033[01;36m选择操作，输入对应的数字即可.\n$options: \033[0m "
+	read -p "" choose
 do
 	echo -e "\n"
 	case $choose in
-	A | a)
+	0)
+		git status;;
+	1)
 		git add -A
 		git status;;
-	C | c)
+	2)
 		echo -e -n "\033[01;36m请输入备注: \033[0m "
 		read commit
 		commitDate="[20"$(date +%y-%m-%d])
 		commit=$commitDate$commit
 		git commit -m "$commit";;
-	D | d)
+	3)
 		git diff HEAD -- *;;
-	E | e)
-		exit 0;;
-	F | f)
+	4)
 		git fetch origin master:origin_master
 		git checkout origin_master
 		git diff master
@@ -79,24 +79,14 @@ do
 			echo -e -n "\033[01;36m取消成功.\n \033[0m "
 		fi
 		git branch -D origin_master;;
-	H | h)
-		echo "	A: 添加所有的文件到缓存区, 对应命令: git add -A. "
-		echo "	C: 确认更改，对应命令：git commit -m 备注. "
-		echo "	D: 对比其他版本和当前版本的区别，对应命令：git diff HEAD --. "
-		echo "	E: 退出脚本，对应命令：exit 0. "
-		echo "	F: 拉取远程分支并合并到本地，对应命令：git fetch && git merge origin_master. "
-		echo "	L: 显示两周内日志记录，对应命令：git log --since=2.weeks. "
-		echo "	P: 推送到远程服务器，对应命令：git push origin master. "
-		echo "	U: 配置用户信息(回车退出), 对应命令：git config "
-		echo "	R: 回滚操作，对应命令：git reset --hard . "
-		echo "	S: 显示当前文件的状态，对应命令：git status. ";;
-	L | l)
+
+	5)
 		echo -e -n "\033[01;36m仅展示两周内日志. \033[0m "
 		git log --since=2.weeks --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit;;
-	P | p)
+	6)
 	        git push origin master
 			git status;;
-	U | u)
+	7)
 		echo -e "\033[01;36m输入配置用户信息的命令并回车, 例如[git config --global user.name 'XXX']. \033[0m "
 		while read -p ">> " gitConfig
 		do
@@ -107,13 +97,42 @@ do
 				break
 			fi
 		done;;
-	R | r)
+	8)
 		git log --since=2.weeks --color --graph --pretty=oneline
 		echo -e -n "\033[01;36m请输入要回滚到的id: \033[0m "
 		read reset_id
-		git reset --hard $reset_id;;
-	S | s)
+		git reset --hard $reset_id
 		git status;;
+	
+	9)
+		echo -e -n "\033[01;36m请输入要回删除的文件名: \033[0m "
+		read rm_file
+		git rm $rm_file
+		git status;;
+	a | A)
+		echo -e -n "\033[01;36m请输入要修改的文件名: \033[0m "
+		read sr_file
+		echo -e -n "\033[01;36m请输入要修改后的文件名: \033[0m "
+		read mv_file
+		git mv $sr_file $mv_file
+		git status;;
+	e | E)
+		exit 0;;
+	h | H)
+		echo "	0: 添加所有的文件到缓存区, 对应命令: git add -A. "
+		echo "	1: 确认更改，对应命令：git commit -m 备注. "
+		echo "	2: 对比其他版本和当前版本的区别，对应命令：git diff HEAD --. "
+		echo "	3: 拉取远程分支并合并到本地，对应命令：git fetch && git merge origin_master. "
+		echo "	4: 显示两周内日志记录，对应命令：git log --since=2.weeks. "
+		echo "	5: 推送到远程服务器，对应命令：git push origin master. "
+		echo "	6: 配置运行自定义命令(回车退出), 对应命令：git config "
+		echo "	7: 回滚操作，对应命令：git reset --hard . "
+		echo "	8: 显示当前文件的状态，对应命令：git status. "
+		echo "	9: 删除指定文件，对应命令：git rm 文件. "
+		echo "	A: 文件改名，对应命令：git mv 文件. "
+		echo "	E: 退出脚本，对应命令：exit 0. "
+		echo "	H: 显示帮助文档. "
+		;;
 	*)
 		echo
 		echo -e -n "\033[31m	亲，别逗我，请按照提示输入.\033[0m"
